fc55668cd9fbcb8600ed5a59ec3bfe6e43d8ffff
jacber01 <jacber2001@gmail.com>
2025-10-07 21:44:27 +0200

    Update Oppgave_2.py

--- Arbeidskrav 2/Oppgave_2.py
+++ Arbeidskrav 2/Oppgave_2.py
@@ -1,6 +1,6 @@
 import math
 
-antall_elever = int(input("Hvor mange elever er det i klassen? ")) # Spør brukeren om antall elever i terminal
+antall_elever = int(input("Hvor mange elever er det i klassen? ")) # Spør om antall elever i terminal
 
 pizza_per_elev = 0.25 # Hver elev spiser 1/4 pizza
 
